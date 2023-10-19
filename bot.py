9ad031a42b5d53a0a25e8135e7b607c38c050756
bar47ney <48147719+bar47ney@users.noreply.github.com>
2023-10-19 22:37:17 +0300

    Add new fixes 2310192236
    Fix code, voice chatgpt and more

--- bot.py
+++ bot.py
@@ -5,13 +5,29 @@ import requests
 import pyttsx3
 import speech_recognition as sr
 import soundfile as sf
+from pydub import AudioSegment
 
 g4f.logging = True # enable logging
 g4f.check_version = False # Disable automatic version checking
 
-bot = telebot.TeleBot('API_KEY')
+bot = telebot.TeleBot('')
 
 engine = pyttsx3.init()
+
+def get_answer(message):       
+    response = g4f.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": f'{message}'}],
+        stream=True,
+    )         
+    response_answer = ""
+    for s in response:
+        response_answer += s
+    return response_answer    
+
+# def load_message(text='Подождите немного...'):
+    # bot.send_message(message.from_user.id, f'{text}', parse_mode='Markdown') # Отправка файла в чат
+
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -24,123 +40,72 @@ def start(message):
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
 
-    if message.text:        
-        response = g4f.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[{"role": "user", "content": f'{message.text}'}],
-            stream=True,
-        )      
-        answer = ""
-        for s in response:
-            # print(message, flush=True, end='')
-            answer += s
-        print(answer)
+    if message.text:  
+        bot.send_message(message.from_user.id, 'Подождите немного...', parse_mode='Markdown') # Отправка файла в чат
         # engine.say(answer)
-
+        answer = get_answer(message.text)
+        bot.send_message(message.from_user.id, f'{answer}', parse_mode='Markdown') # Отправка файла в чат
         # Воспроизводим речь
         engine.save_to_file(f'{answer}', 'answer.wav')
         engine.runAndWait()  
-        bot.send_message(message.from_user.id, f'{answer}', parse_mode='Markdown') # Отправка файла в чат
         bot.send_document(message.from_user.id, document=open("answer.wav", "rb"))
         # bot.send_audio(message.from_user.id, audio=open("answer.wav", "rb"))
-
-    #ответ бота  
-    #     markup = types.ReplyKeyboardMarkup(resize_keyboard=True) #создание новых кнопок
-    #     btn1 = types.KeyboardButton('Как стать автором на Хабре?')
-    #     btn2 = types.KeyboardButton('Правила сайта')
-    #     btn3 = types.KeyboardButton('Советы по оформлению публикации')
-    #     markup.add(btn1, btn2, btn3)
-    #     bot.send_message(message.from_user.id, '❓ Задайте интересующий вас вопрос', reply_markup=markup) #ответ бота
-
-
-    # elif message.text == 'Как стать автором на Хабре?':
-    #     bot.send_message(message.from_user.id, '[ссылка](https://habr.com/ru/sandbox/start/)', parse_mode='Markdown')
-
-    # elif message.text == 'Правила сайта':
-    #     bot.send_message(message.from_user.id, 'Прочитать правила сайта вы можете по ' + '[ссылке](https://habr.com/ru/docs/help/rules/)', parse_mode='Markdown')
-
-    # elif message.text == 'Советы по оформлению публикации':
-    #     bot.send_message(message.from_user.id, 'Подробно про советы по оформлению публикаций прочитать по ' + '[ссылке](https://habr.com/ru/docs/companies/design/)', parse_mode='Markdown')
-# from pydub import AudioSegment
-
-# # Конвертируем OGG-файл в WAV-формат
-# def convert_ogg_to_wav(ogg_file, wav_file):
-#     audio = AudioSegment.from_file(ogg_file, format='ogg')
-#     audio.export(wav_file, format='wav')
-
-# # Путь к оригинальному OGG-файлу
-# ogg_file_path = 'voice_message.ogg'
-# # Путь для сохранения WAV-файла
-# wav_file_path = 'voice_message.wav'
-
-# # Конвертируем OGG-файл в WAV-формат
-# convert_ogg_to_wav(ogg_file_path, wav_file_path)
-
-from pydub import AudioSegment
     
-global_text = ''
-
 @bot.message_handler(content_types=['voice'])
 def handle_voice_message(message):
+    bot.send_message(message.from_user.id, 'Подождите немного...', parse_mode='Markdown') # Отправка файла в чат
     # Получаем файл голосового сообщения
     voice = message.voice
     file_info = bot.get_file(voice.file_id)
     file = bot.download_file(file_info.file_path)
-    # Сохраняем файл на диск
     with open('voice_answer.wav', 'wb') as f:
         f.write(file)
-#     # Распознаем речь из файла
-#     recognizer = sr.Recognizer() 
-    
-#     with sr.AudioFile('voice_message.wav') as source:
-#         audio = recognizer.record(source)
-#     try:
-#         # Используем Google Web Speech API для распознавания речи
-#         text = recognizer.recognize_google(audio, language='ru-RU')
-#         bot.reply_to(message, text)
-#     except sr.UnknownValueError:
-#         bot.reply_to(message, "Не удалось распознать голосовое сообщение")
-#     except sr.RequestError as e:
-#         bot.reply_to(message, "Ошибка сервиса распознавания речи; {0}".format(e))
-    # Создание экземпляра объекта Recognizer
-
-    # Путь к исходному файлу
     input_file = 'voice_answer.wav'
-
-    # Путь к файлу в новом формате
     output_file = 'voice_answer.flac'
-
-    # Загрузка аудиофайла
     audio_data, sample_rate = sf.read(input_file)
-
-    # Запись аудиофайла в новом формате (FLAC)
     sf.write(output_file, audio_data, sample_rate, format='FLAC')
-
-
-
     r = sr.Recognizer()
-    # Открытие аудиофайла с помощью AudioFile
     with sr.AudioFile('voice_answer.flac') as source:
-        # Чтение аудиоданных из файла
         audio_data = r.record(source)
-        # Распознавание речи
         text = r.recognize_google(audio_data, language='ru')
-        # Вывод распознанного текста
-        print(text)
-        global_text = text
-        bot.send_message(message.from_user.id, f'{text}', parse_mode='Markdown') # Отправка файла в чат
 
+        bot.send_message(message.from_user.id, f'Ищу "{text}..."', parse_mode='Markdown') # Отправка файла в чат
+
+        answer_from_voice = get_answer(text)
+        bot.send_message(message.from_user.id, f'{answer_from_voice}', parse_mode='Markdown') # Отправка файла в чат
         
-        response2 = g4f.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[{"role": "user", "content": f'{global_text}'}],
-            stream=True,
-        )      
-        answer2 = ""
-        for ss in response2:
-            # print(message, flush=True, end='')
-            answer2 += ss
-        bot.send_message(message.from_user.id, f'{answer2}', parse_mode='Markdown') # Отправка файла в чат
+        
+        bot.send_message(message.from_user.id, 'Проговорю вам ответ...', parse_mode='Markdown') # Отправка файла в чат
+        engine.save_to_file(f'{answer_from_voice}', 'answer_from_voice.wav')
+        engine.runAndWait()  
+        bot.send_document(message.from_user.id, document=open("answer_from_voice.wav", "rb"))
+
+# Обработчик аудиосообщений
+@bot.message_handler(content_types=['audio'])
+def handle_audio(message):
+    # Получение информации о файле
+    file_info_audio = bot.get_file(message.audio.file_id)
+    file_audio = bot.download_file(file_info_audio.file_path)
+
+    # Сохранение файла на диск
+    with open('audio_file.wav', 'wb') as audio_file:
+        audio_file.write(file_audio)
+    
+    input_file_audio = 'audio_file.wav'
+    output_file_audio = 'audio_file.flac'
+    audio_data_audio, sample_rate_audio = sf.read(input_file_audio)
+    sf.write(output_file_audio, audio_data_audio, sample_rate_audio, format='FLAC')    
+
+    # Преобразование аудио в текст
+    r = sr.Recognizer()
+    with sr.AudioFile('audio_file.flac') as source:
+        audio_data = r.record(source)
+        audio_file_text = r.recognize_google(audio_data, language='ru')  # Здесь можно указать нужный язык
+
+    # Отправка текстового сообщения
+    
+    print(audio_file_text)
+    bot.send_message(message.from_user.id, f'{audio_file_text}', parse_mode='Markdown') # Отправка файла в чат
 
 bot.polling(none_stop=True, interval=0) 
 #обязательная для работы бота часть
